4c53e12ffebf1c603cf65a7c970c557631f02c4e
Raimundo Matias <matias@g3.3759.hakusho>
2019-02-23 01:50:29 -0300

    Aula 02 - Inserindo no banco de dados

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # PHP e MySQL I: Fundamentos para criar um sistema na Web
 
-## Aula 01 : Primeiros passos com PHP
+## Aula 02 : Inserindo Produtos no banco de dados.
 
-1. Primeiro passos com PHP
-2. PHP, sintaxe básica I
-3. Praticando a sintaxe I
-4. PHP, sintaxe básica II
-5. Praticando a sintaxe II
-7. Criando o primeiro HTML
-8. Primeira versão do adiciona-produto
-9. Exibindo preço
-10. Criando formulário
-11. Cabeçalho e rodapé
+1. Criando a tabela de produtos
+2. Inserindo algusn produtos
+3. Inserindo no banco de dados
+4. fechando a conexão
+5. Os "i" nos metodos
--- adiciona-produto.php
+++ adiciona-produto.php
@@ -3,9 +3,23 @@
 <?php
     $nome = $_GET['nome'];
     $preco = $_GET['preco'];
+
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+
+    $conexao = mysqli_connect("127.0.0.1", "root", "1q2w3e4r", "loja");
+
+    if (mysqli_query($conexao, $query)) { ?>
+        <p class="alert-success">
+            Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
+        </p>
+
+    <?php } else { ?>
+        <p class="alert-danger">
+            O Produto <?= $nome; ?> não foi adicionado.
+        </p>
+    <?php }
+    
+    mysqli_close($conexao);
 ?>
-<p class="alert-success">
-    Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
-</p>
 
 <?php include("rodape.php"); ?>
